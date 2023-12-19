ed1fea517fd5fe9d2638f128c3a29fb980b59fb1
19sandip <sandeep307098@gmail.com>
2023-12-19 14:04:40 +0530

    this is module 4 assignment

---
+++ full-javascript/mod-4-assignment/script1.js
@@ -0,0 +1,9 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "jim"];
+  
+for (var i = 0; i < names.length; i++) {
+ if (names[i].charAt() === "j" || names[i].charAt()=== "J") {
+  console.log("Good Bye  " + names[i])
+ } else {
+  console.log("Hello  " + names[i]);
+ }
+}
